834d20d850d149cf44e9bdcaf5fad9cde0b2e182
Ali Bilgin Arslan <aarslan@Ford-Prefect.local>
2011-08-19 14:49:47 -0400

    major restructure 1

--- face2dto3d_CSHA.m
+++ face2dto3d_CSHA.m
@@ -1,36 +1,11 @@
-% hmax_cvpr06_run_cal101 - Demo script that performs multiclass classification.
-%
-% This script runs hmax against the Caltech 101 dataset using the model
-% configuration of [Mutch & Lowe 2006].  With minor modifications it could run
-% on any similar dataset.
-%
-% The script does everything in the paper (for the multiclass problem) except
-% the final feature selection step.  (The feature selection step doesn't improve
-% the classification score much -- it just reduces the number of features needed
-% for the model.)
-%
-% You will have to copy and edit this script to provide the path of the Caltech
-% 101 (or similar) dataset on your system.
-%
-% You might also want to insert "save" commands at appropriate points in this
-% script, as it will take some time to run.
-%
-% See hmax_cvpr06_run_simple for more comments on individual command usage.
-%
-% See also: hmax_cvpr06_run_simple, hmax_cvpr06_run_uiuc.
 
-%-----------------------------------------------------------------------------------------------------------------------
+function face2dto3d_CSHA(runMode, varargin)
+%runMode: extract patches for multiple face: 0
+%         extract patches for one face     : 1
 
-% while true
-%     ans = lower(strtrim(input('All variables will be cleared.  Is this okay (y/n) ? ', 's')));
-%     if isempty(ans), continue; end
-%     if ans(1) == 'y', break; end
-%     if ans(1) == 'n', return; end
-% end
-
-function face2dto3d_CSHA(varargin)
-
-fprintf('\n');
+if ~exist('runMode', 'var')
+    runMode = 0;
+end
 
 mystream = RandStream('mt19937ar','Seed',sum(100*clock));
 RandStream.setDefaultStream(mystream);
@@ -43,29 +18,23 @@ if strcmp(computer, 'MACI64')
 else
     dataPath = '/gpfs/home/aarslan/work/PART_staticFaceDataBase';
 end
+if ~exist(dataPath)
+    error('I couldnt find anything in the datapaths provided');
+end
 
 %addpath('/Users/aarslan/Documents/MATLAB/cns/hmax/face_lfw')
 %p = hmax_cvpr06_params_full_lfw;  % Model configuration to use.  Note that this script assumes that the only stage having
 
 
-numFeatures = 4096;           % Number of S2 features to learn.
-numPat = 40000;
+numPat = 1000;
 patSize = 50;
 downSamSiz = [9 9];
 oriMode = 1;                    % 1= mean orientation in the patch, else = mid point in the patch
 numPics = 1;                  %numel(origPicPaths)
-numTrain    = 8;             % Number of training images per category.
-maxTest     = inf;            % Maximum number of test images per category.
-minSetSize  = 15;              % minimum number of images for a person required to include that person in the classification.
 okBands     = 2:2:8;
-regularPatching = 1;
+regularPatching = 0;
+patMode = 1;
 
-
-%-----------------------------------------------------------------------------------------------------------------------
-
-if isempty(dataPath)
-    error('you must edit this script to supply the path of the dataset (variable "dataPath")');
-end
 %-----------------------------------------------------------------------------------------------------------------------
 origDirs = dir([dataPath '/*orig.png']);
 normDirs = dir([dataPath '/*normals.png']);
@@ -80,7 +49,7 @@ normPicPaths = cellfun(@(paths,pics) [paths '/' pics(1:end)], normPicPaths(:,1),
 
 load filters.mat
 rot = linspace(0, 180, 13);
-RF_siz    = [7:2:39];
+RF_siz    = 7:2:39;
 Div       = linspace(4,3.2,numel(RF_siz));%[4:-.05:3.2];
 display('Initializing gabor filters -- full set...');
 %creates the gabor filters use to extract the S1 layer
@@ -88,7 +57,6 @@ display('Initializing gabor filters -- full set...');
 
 s1vec = cell(numPics, numPat, numel(rot));
 truth = cell(numPics, numPat);
-rects = cell(numPics, numPat);
 origPats  = cell(numPics, numPat);
 
 randomizer = randperm(numel(origPicPaths));
@@ -99,6 +67,7 @@ if regularPatching
 [patOfPic randomizer] = pickBestFace(results, randomizer);
 end
 
+
 for i = 1:numPics
     
     fprintf('%u/%u: computing s1 vector for %s\n', i, numPics, origPicPaths{randomizer(i)});
@@ -107,16 +76,16 @@ for i = 1:numPics
     imNorm = imread(normPicPaths{randomizer(i)});
     im = rgb2gray(im);
     [ s1] = getS1C1_legacy(im, fSiz,filters,c1OL,numSimpleFilters );
-    regPat.imSize = size(im);
-    regPat.numPat = numPat;
-    regPat.patSize = patSize;
+    
+    pSpec.imSize = size(im);
+    pSpec.patSize = patSize;
+    pSpec.patMode = patMode; %patch mode: 0 random; 1 regular spaced.
+    pSpec.numPat = numPat;
+    
+    [rects] = prepAllPatches(pSpec);
+    numPat = size(rects, 1);
+    
     for j=1:numPat
-        j
-        if regularPatching
-            [rect rects] = prepRegularPatches(regPat, j);
-        else
-            [ rect] = prepPatches(im, patSize);
-        end
         origPat = imcrop(im, rect);
         normPat = imcrop(imNorm, rect);
         [x y z] = getAveOri(normPat, oriMode);
@@ -136,20 +105,54 @@ end
 
 end
 
-function [ s1vec] = getS1C1_legacy(im, fSiz,filters,c1OL,numSimpleFilters)
-rot = linspace(0, 180, 13);
-c1ScaleSS = [1:2:18];
-RF_siz    = [7:2:39];
-c1SpaceSS = [8:2:22];
-minFS     = 7;
-maxFS     = 39;
-minFS     = 7;
-maxFS     = 39;
-Div       = linspace(4,3.2,numel(RF_siz));%[4:-.05:3.2];
-
+function [ s1vec] = getS1C1_legacy(im, fSiz,filters,c1OL,~)
+c1ScaleSS = 1:2:18;
+c1SpaceSS = 8:2:22;
 [s1vec] = C1(im, filters, fSiz, c1SpaceSS, c1ScaleSS, c1OL, 1);
 
 end
+
+function [ rects] = prepAllPatches(pSpec)
+imSize  = pSpec.imSize;
+patSize = pSpec.patSize;
+patMode   = pSpec.patMode; %patch mode: 0 random; 1 regular spaced.
+numPat  = pSpec.numPat;
+
+rects = zeros(numPat,4);
+
+switch patMode
+    case 0
+        rects(:,1) = randi(imSize(1) - patSize,numPat,1);
+        rects(:,2) = randi(imSize(2) - patSize,numPat,1);
+    case 1
+        
+        facts = imSize/min(imSize); %create a series of pairs to find the ratios for edges
+        veli = repmat(facts, [], prod(imSize));
+        ali= repmat(1:prod(imSize), [], 2)';
+        
+        axsDims = ceil((find(prod(ali.*veli,2) <= numPat,1, 'last')) *facts);
+        
+        if (axsDims(2)+1)* axsDims(1) <= numPat
+            axsDims = [axsDims(1) axsDims(2)+1];
+        elseif (axsDims(1)+1)* axsDims(2) <= numPat %check if we can increase the number a bit more
+            axsDims = [axsDims(1)+1 axsDims(2)];
+        end
+        
+        [x y] = meshgrid(linspace(1, imSize(1)-patSize, axsDims(1)), linspace(1, imSize(2)-patSize, axsDims(2))); % create xy pairs
+        finNumP = prod(axsDims);
+        
+        rects(1:finNumP,1:2) = [x(1:finNumP); y(1:finNumP)]'; %put in rect & trim
+        rects = rects(1:finNumP,:);
+        if finNumP ~= numPat
+            warning(sprintf('you wanted %d patches, but I could fit in only %d', numPat,  finNumP ))
+        end
+end
+
+rects(:,3) = patSize;
+rects(:,4) = patSize;
+
+end
+
 
 function [ rect] = prepPatches(im, patSize, varargin)
 %patSize: the edge size of a square patch
@@ -176,6 +179,20 @@ end
 end
 
 
+function  ali = preValidPatches(im, imNorm, rects)
+
+    for j=1:numPat
+        origPat = imcrop(im, rect);
+        normPat = imcrop(imNorm, rect);
+        [x y z] = getAveOri(normPat, oriMode);
+        truth{i,j} = [x, y , z];
+        s1vec(i,j,:) = vectorizeS1(s1,rect, okBands, downSamSiz);
+        origPats{i,j} = imresize(origPat, downSamSiz);
+        rects{i,j} = rect;
+    end
+
+end
+
 function [thisRect rects] = prepRegularPatches(regPar, it)
 
 
@@ -187,7 +204,7 @@ while pro >= regPar.numPat
     pro = (700-y)*(600-y);
     y=y+1;
 end
-wArr = ceil(linspace(regPar.patSize/2, w-regPar.patSize/2, w-y));
+wArr = ceil(linspace(regPar.patSize/2, w-regPar.patSize/2, w-y)); %use meshgrid
 hArr = ceil(linspace(regPar.patSize/2, h-regPar.patSize/2, h-y));
 cnt = 1;
 for ws=1: w-y
@@ -231,7 +248,7 @@ if Mode == 1 %take the mean orientation
     y = mean(mean(y));
     z = mean(mean(z));
 else % take the central orientation
-    dims = floor(size(normPat));
+    dims = floor(size(normPat)/2);
     x = x(dims(1), dims(2));
     y = y(dims(1), dims(2));
     z = z(dims(1), dims(2));
