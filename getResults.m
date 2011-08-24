442b147340faa310d5677011c68ba85d302b91cd
Ali Bilgin Arslan <aarslan@Ford-Prefect.local>
2011-08-24 11:03:57 -0400

    tidied up the regression a bit

--- getResults.m
+++ getResults.m
@@ -1,9 +1,10 @@
 
 results = cell(3,8);
+resultsPerm = cell(3,8);
 for or=1:3
     or
    parfor cv=1:8
        cv
-       results{or,cv} = doRegression(origPats, s1vec, truth, or);
+       [results{or,cv} resultsPerm{or,cv}] = doRegression(origPats, s1vec, truth, or);
    end
 end
