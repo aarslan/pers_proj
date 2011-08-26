e2f691dcc7c9c17cdac150e1e23894c20032b698
Ali Bilgin Arslan <aarslan@Ford-Prefect.local>
2011-08-26 10:17:02 -0400

    adjusted regression parameters

--- getResults.m
+++ getResults.m
@@ -6,7 +6,7 @@ resultsPerm = cell(3,8);
 rand('state',sum(100*clock))
 
 parfor cv=1:8
-    ali = load(patchesAll.mat);
+    ali = load('patchesAll.mat');
     swirler = randi(5000);
     for or=1:3
         display(['cv: '  mat2str(cv) ', ori: ' mat2str(or)])
